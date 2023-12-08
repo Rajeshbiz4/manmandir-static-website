545c9b8feeb6c4b76cf229dcdafe82f6394bf384
rajeshbiz4 <rajesh.pandhare181@gmail.com>
2023-12-08 23:59:45 +0530

    lint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
 import Gallary from "./pages/gallary";
 import Contact from "./pages/contact";
 import Login from "./pages/login/login";
 import Home from "./pages/home";
-import PrivateLayout from './components/Layout/private-layout';
+// import PrivateLayout from './components/Layout/private-layout';
 import PublicLayout from "./components/Layout/public-layout";
 
 import "./App.css";
 
 const App = () => {
 
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <>
