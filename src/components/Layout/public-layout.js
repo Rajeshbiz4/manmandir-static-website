b85b56aba74d5d53ca306e83ce3c6ad296fd39d5
rajeshbiz4 <rajesh.pandhare181@gmail.com>
2023-12-08 23:48:57 +0530

    babel

--- src/components/Layout/public-layout.js
+++ src/components/Layout/public-layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import NavBar from '../navbar';
 import WhatsAppButton from '../whatsappButton';
 import Footer from '../footer';
-import ContactDetailBar from '../contact-details-bar';
 
 const PublicLayout = (props) => (
     <>
